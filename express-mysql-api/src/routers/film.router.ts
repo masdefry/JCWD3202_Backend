82d9897c189f18310ad89806504bb6151143ed83
masdefry <muhammaddefryan@gmail.com>
2025-03-20 10:58:22 +0700

    create transaction member

--- express-mysql-api/src/routers/film.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const filmRouter = Router();
-import { findFilmById, findFilms } from '../controllers/film.controller';
-
-filmRouter.get('/', findFilms);
-filmRouter.get('/:id', findFilmById)
-
-export default filmRouter;
